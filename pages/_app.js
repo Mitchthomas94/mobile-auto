645d1992cc41a07e747a3af0eaa24655a3d6c563
Mitchthomas94 <hello@autobookedai.co.uk>
2025-08-24 13:36:51 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-
+// pages/_app.js
 import '../styles/globals.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -6,7 +6,19 @@ import CookieBanner from '../components/CookieBanner'
 import AnalyticsGate from '../components/AnalyticsGate'
 import MobileStickyCTA from '../components/MobileStickyCTA'
 
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  // 🔒 Safety: if anything ever hits /how, send it to /how-it-works
+  useEffect(() => {
+    if (router.asPath === '/how') {
+      router.replace('/how-it-works')
+    }
+  }, [router.asPath])
+
   return (
     <>
       <Header/>
@@ -14,8 +26,9 @@ export default function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </main>
       <AnalyticsGate/>
-      <CookieBanner/><Footer/>
-          <MobileStickyCTA/>
+      <CookieBanner/>
+      <Footer/>
+      <MobileStickyCTA/>
     </>
   )
 }
