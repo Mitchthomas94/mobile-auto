b6e7e4f430aa82bf42f13e570e43fae45dc651ef
Mitchthomas94 <hello@autobookedai.co.uk>
2025-08-24 12:36:27 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -17,12 +17,14 @@ export default function Header() {
 
         {/* Links */}
         <nav className="nav-links" aria-label="Primary">
-          <a href="/">Home</a>
-          <a href="/how-it-works">How It Works</a>   {/* ✅ fixed link */}
-          <a href="/pricing">Pricing</a>
-          <a href="/industries">Industries</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a>
+  <a href="/">Home</a>
+  <a href="/how-it-works">How It Works</a>   {/* ✅ correct */}
+  <a href="/pricing">Pricing</a>
+  <a href="/industries">Industries</a>
+  <a href="/about">About</a>
+  <a href="/contact">Contact</a>
+</nav>
+
         </nav>
 
         {/* CTA */}
